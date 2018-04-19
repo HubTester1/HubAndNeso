2c112d337ef02590e03849e65016276b2d6bfc97
James T Baker <skipbaker@protonmail.com>
2018-04-19 14:56:59 -0400

    Setup phase

--- src/reactStartSansSP.js
+++ src/reactStartSansSP.js
@@ -6,26 +6,23 @@ import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import './styles/styles.sass';
 
 class StartPage extends React.Component {
-
 	// state can be set sans constructor
 	state = {
-		buttonText: "This is button text"
+		buttonText: 'This is some long button text',
 	};
-
-	// other properties can also be set directly on the class
-	paragraphText = "This is some sweet para text, yo.";
-	name = "Slim Shady";
 
 	// arrow functions can be class properties; arrow functions 
 	// 		don't have their own this binding, so they needn't 
 	// 		be manually bound
-	getIntroduction = () => {
-		return `Hi, my name is ${this.name}`;
-	}
+	getIntroduction = () => `Hi, my name is ${this.name}`
+
+	// other properties can also be set directly on the class
+	paragraphText = 'This is some sweet para text, yo.';
+	name = 'Slim Shady';
 
 	render() {
 		const intro = this.getIntroduction();
-		return(
+		return (
 			<Fabric>
 				<p>{intro}</p>
 				<p>{this.paragraphText}</p>
