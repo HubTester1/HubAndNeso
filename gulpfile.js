bc0d31f0df524a73f3f4e7cab57c605f72b3774d
James T Baker <skipbaker@protonmail.com>
2018-04-19 14:52:27 -0400

    Set up phase

--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,16 @@ dotenv.config({ path: './.env' });
 
 const gulp = require('gulp');
 const spsave = require('gulp-spsave');
-// const watch = require('gulp-watch');
 const cached = require('gulp-cached');
 const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const util = require('gulp-util');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const webpackConfig = require('./webpack.config.js');
-// const webpackDevServer = require('webpack-dev-server');
 
 // ----- CONFIG SETTINGS
 
-const srcFolder = 'src';
-const distFolder = 'dist';
+const srcFolder = './src';
+const distFolder = './dist';
 
 const gulpSPSaveDevOptions = {
 	siteUrl: 'https://bmos.sharepoint.com/sites/hubdev',
@@ -42,31 +38,6 @@ const gulpSPSaveCredentials = {
 };
 
 // ----- CONFIG TASKS
-
-// gulp.task('XXXXXXXXXXXXXX', () => {
-// 
-// });
-
-
-// run webpack dev-server and refresh local browser on changes
-// webpack dev-server compiles to cache, does not write a file
-// do this for most of development
-gulp.task('serve', () => {
-
-});
-
-/* const gulpPlumberObj = {
-	// pass in errorHandler property
-	errorHandler(err) {
-		notify.onError({
-			title: `Gulp error in ${err.plugin}`,
-			message: err.toString(),
-		})(err);
-
-		// play a sound once
-		util.beep();
-	},
-}; */
 
 // build dist file
 gulp.task('build', () => {
