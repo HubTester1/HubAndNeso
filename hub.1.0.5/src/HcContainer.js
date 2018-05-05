00680106c59a4e13e73fa8e3c34435759eeb4f54
James T Baker <skipbaker@protonmail.com>
2018-05-05 13:59:16 -0400

    Dev phase

--- hub.1.0.5/src/HcContainer.js
+++ hub.1.0.5/src/HcContainer.js
@@ -5,6 +5,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './sass/main.sass';
 
+import EnvironmentDetector from './services/EnvironmentDetector';
 import MOSUtilities from './services/MOSUtilities';
 
 import HcContainerData from './HcContainerData';
@@ -26,38 +27,43 @@ class HcContainer extends React.Component {
 		uData: {},
 	};
 	componentDidMount() {
-		HcContainerData.ReturnUData()
-			.then((response) => {
-				const accountBrief = 
-					MOSUtilities.ReplaceAll('i:0#.f\\|membership\\|', '', MOSUtilities.ReplaceAll('@mos.org', '', response.LoginName.toLowerCase()));
-				this.setState(() => ({
-					uData: {
-						email: response.Email,
-						account: accountBrief,
-						displayName: response.Title,
-					},
-				}));
-			})
-			.catch((error) => {
-				// 
-			});
+		if (EnvironmentDetector.ReturnIsHCScreen()) {
+			HcContainerData.ReturnUData()
+				.then((response) => {
+					const accountBrief = 
+						MOSUtilities.ReplaceAll('i:0#.f\\|membership\\|', '', MOSUtilities.ReplaceAll('@mos.org', '', response.LoginName.toLowerCase()));
+					this.setState(() => ({
+						uData: {
+							email: response.Email,
+							account: accountBrief,
+							displayName: response.Title,
+						},
+					}));
+				})
+				.catch((error) => {
+					// 
+				});
+		}
 	}
 	render() {
-		return (
-			<div>
-				<HcTopCommandBar />
-				<HcStaffLookup />
-				<HcPushedItems />
-				<HcGetItDone />
-				<HcOrganization />
-				<HcMessages
-					uData={this.state.uData}
-				/>		
-				{/* <p>HcCalendarsSchedules here</p> */}
-			</div>
-		);
+		if (EnvironmentDetector.ReturnIsHCScreen()) {
+			return (
+				<div>
+					<HcTopCommandBar />
+					<HcStaffLookup />
+					<HcPushedItems />
+					<HcGetItDone />
+					<HcOrganization />
+					<HcMessages
+						uData={this.state.uData}
+					/>		
+					{/* <p>HcCalendarsSchedules here</p> */}
+				</div>
+			);
+		}
+		return null;
 	}
 }
 
 
-ReactDOM.render(<HcContainer />, document.getElementById('DeltaPlaceHolderMain'));
+ReactDOM.render(<HcContainer />, document.getElementById('s4-bodyContainer'));
--- hub.1.0.5/src/services/EnvironmentDetector.js
+++ hub.1.0.5/src/services/EnvironmentDetector.js
@@ -1,4 +1,7 @@
 
+
 module.exports = {
 	ReturnIsSPO: () => window.location.hostname.indexOf('sharepoint') > -1,
+	ReturnIsHCScreen: () => window.location.pathname.toLowerCase().indexOf('app.aspx') > -1 ||
+			window.location.hostname.indexOf('192') > -1,
 };
