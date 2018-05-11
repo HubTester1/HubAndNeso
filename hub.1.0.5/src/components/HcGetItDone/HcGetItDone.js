b729ad149e1233d69e87808349a7da415cd39d03
James T Baker <skipbaker@protonmail.com>
2018-05-11 19:14:39 -0400

    Dev phase

--- hub.1.0.5/src/components/HcGetItDone/HcGetItDone.js
+++ hub.1.0.5/src/components/HcGetItDone/HcGetItDone.js
@@ -2,6 +2,12 @@
 // ----- IMPORTS
 
 import * as React from 'react';
+import {
+	AccordionItem,
+	AccordionItemTitle,
+	AccordionItemBody,
+} from 'react-accessible-accordion';
+
 import HcGetItDoneData from './HcGetItDoneData';
 import HcGetItDoneCommandBar from './components/HcGetItDoneCommandBar/HcGetItDoneCommandBar';
 import HcGetItDoneViewByAlpha from './components/HcGetItDoneViewByAlpha/HcGetItDoneViewByAlpha';
@@ -25,6 +31,7 @@ export default class HcGetItDone extends React.Component {
 		};
 		this.handleClickViewByAlphaButton = this.handleClickViewByAlphaButton.bind(this);
 		this.handleClickViewByGroupButton = this.handleClickViewByGroupButton.bind(this);
+		this.returnHcGetItDoneBody = this.returnHcGetItDoneBody.bind(this);
 	}
 	componentDidMount() {
 		HcGetItDoneData.ReturnAllGetItDoneData()
@@ -54,40 +61,76 @@ export default class HcGetItDone extends React.Component {
 			showViewByGroup: true,
 		}));
 	}
-	render() {
+	returnHcGetItDoneBody() {
 		return (
+			<div id="hc-get-it-done-body">
+				<HcGetItDoneCommandBar
+					handleClickViewByGroupButton={this.handleClickViewByGroupButton}
+					handleClickViewByAlphaButton={this.handleClickViewByAlphaButton}
+				/>
+				{
+					this.state.showViewByGroup &&
+
+					<HcGetItDoneViewByGroup
+						listItemsGroupedArray={this.state.listItemsGroupedArray}
+					/>
+				}
+				{
+					this.state.showViewByAlpha &&
+
+					<HcGetItDoneViewByAlpha
+						listItemsAlphaArray={this.state.listItemsAlphaArray}
+					/>
+				}
+			</div>
+		);
+	}
+	render() {
+		if (!this.state.queryError && this.props.screenType === 'small') {
+			return (
+				<AccordionItem
+					id="hc-get-it-done"
+					className="hc-get-it-done mos-react-component-root accordion__item"
+					hideBodyClassName="accordion__item--hidden"
+					name="hc-get-it-done"
+				>
+					<AccordionItemTitle
+						className="hc-get-it-done__title accordion__title"
+					>
+						<h2>Get it Done</h2>
+					</AccordionItemTitle>
+					<AccordionItemBody
+						className="hc-get-it-done__body accordion__body"
+					>
+						{this.returnHcGetItDoneBody()}
+					</AccordionItemBody>
+				</AccordionItem>
+			);
+		}
+		if (!this.state.queryError && this.props.screenType === 'medium') {
+			return (
+				<div id="hc-get-it-done" className="mos-react-component-root" name="hc-get-it-done">
+					<h2>Get it Done</h2>
+					{this.returnHcGetItDoneBody()}
+				</div>
+			);
+		}
+		return (
+			<p id="hc-get-it-done-body">I can&apos;t show you this information right now.</p>
+		);
+		/* return (
 			<div id="hc-get-it-done" className="mos-react-component-root" name="hc-get-it-done">
 				<h2>Get it Done</h2>
 				{
-					!this.state.queryError &&
-
-					<div id="hc-get-it-done-body">
-						<HcGetItDoneCommandBar
-							handleClickViewByGroupButton={this.handleClickViewByGroupButton}
-							handleClickViewByAlphaButton={this.handleClickViewByAlphaButton}
-						/>
-						{
-							this.state.showViewByGroup &&
-
-							<HcGetItDoneViewByGroup
-								listItemsGroupedArray={this.state.listItemsGroupedArray}
-							/>
-						}
-						{
-							this.state.showViewByAlpha &&
-
-							<HcGetItDoneViewByAlpha
-								listItemsAlphaArray={this.state.listItemsAlphaArray}
-							/>
-						}
-					</div>
+					!this.state.queryError && 
+					this.returnHcGetItDoneBody()
 				}
 				{
 					this.state.queryError &&
 
-					<p id="hc-get-it-done-body">I can&apos;t show you this information right now.</p>
+					
 				}
 			</div>
-		);
+		); */
 	}
 }
