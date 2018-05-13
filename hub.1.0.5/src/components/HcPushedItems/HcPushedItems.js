d96fce20df6a2a773ef0462622ab2c4a008af19b
James T Baker <skipbaker@protonmail.com>
2018-05-13 12:46:46 -0400

    Dev phase

--- hub.1.0.5/src/components/HcPushedItems/HcPushedItems.js
+++ hub.1.0.5/src/components/HcPushedItems/HcPushedItems.js
@@ -2,6 +2,11 @@
 // ----- IMPORTS
 
 import * as React from 'react';
+import {
+	AccordionItem,
+	AccordionItemTitle,
+	AccordionItemBody,
+} from 'react-accessible-accordion';
 import HcPushedItemsData from './HcPushedItemsData';
 import HcPushedItemsLink from './HcPushedItemsLink';
 
@@ -26,9 +31,40 @@ export default class HcPushedItems extends React.Component {
 	}
 
 	render() {
+		if (this.props.screenType === 'small') {
+			return (
+				<AccordionItem
+					id="hc-pushed-items"
+					className="hc-pushed-items mos-react-component-root accordion__item"
+					hideBodyClassName="accordion__item--hidden"
+					name="hc-pushed-items"
+				>
+					<AccordionItemTitle
+						className="hc-pushed-items__title accordion__title"
+					>
+						<h2>Please Be Aware</h2>
+					</AccordionItemTitle>
+					<AccordionItemBody
+						className="hc-pushed-items__body accordion__body"
+					>
+						<ul id="hc-pushed-items-list">
+							{
+								this.state.listItemsArray.map((listItemValue, listItemIndex) => (
+									<HcPushedItemsLink
+										key={listItemValue.key}
+										listItemId={listItemValue.key}
+										listItemContent={listItemValue}
+									/>
+								))
+							}
+						</ul>
+					</AccordionItemBody>
+				</AccordionItem>
+			);
+		}
 		return (
 			<div id="hc-pushed-items" className="mos-react-component-root" name="hc-pushed-items">
-				<h3>Please Be Aware</h3>
+				<h2>Please Be Aware</h2>
 				<ul id="hc-pushed-items-list">
 					{
 						this.state.listItemsArray.map((listItemValue, listItemIndex) => (
@@ -42,5 +78,5 @@ export default class HcPushedItems extends React.Component {
 				</ul>
 			</div>
 		);
-	}
+	}	
 }
