5b0502ffec4d9371535d798f0909e04f2f3f850c
James T Baker <skipbaker@protonmail.com>
2018-05-21 15:01:14 -0400

    Dev phase

--- webpack/v4.dev.css.config.js
+++ webpack/v4.dev.css.config.js
@@ -5,23 +5,36 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 // eslint-disable-next-line
 const HtmlWebpack = require('html-webpack-plugin');
+// eslint-disable-next-line
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const baseConfig = require('./base.config.js');
 const path = require('path');
 
 module.exports = merge(baseConfig, {
+	mode: 'production',
 	entry: {
 		index: './hub.1.0.4/sass/mos.sass',
 	},
 	output: {
 		path: path.join(__dirname, '../hub.1.0.4/css'),
-		filename: 'mos.css',
+		filename: 'mos.css.js',
 	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: 'mos.css',
+			// chunkFilename: '[id].css',
+		}),
+	],
 	module: {
 		rules: [
 			{
 				include: path.join(__dirname, '../hub.1.0.4/sass'),
 				test: /\.sass$/,
 				use: [
+					{ loader: MiniCssExtractPlugin.loader },
+					{ loader: 'css-loader' },
 					{ loader: 'postcss-loader' },
 					{ loader: 'sass-loader' },
 				],
