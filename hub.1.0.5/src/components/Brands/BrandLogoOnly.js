42a603a94b85a6de46c65718622466ad4e9a94e3
James T Baker <skipbaker@protonmail.com>
2018-06-04 15:42:23 -0400

    Dev phase

--- hub.1.0.5/src/components/Brands/BrandLogoOnly.js
+++ hub.1.0.5/src/components/Brands/BrandLogoOnly.js
@@ -8,14 +8,18 @@ import * as React from 'react';
 const BrandLogoOnly = props =>
 	(
 		<div className="svg-brand--logo-only mos-react-component-root">
-			<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 118 118" enableBackground="new 0 0 118 118" xmlSpace="preserve">
-				<path fill="#FEBE00" d="M41.8 57.4L2.5 34.7 41.8 12 41.8 57.4z" />
-				<path fill="#AC59FF" d="M41.8 57.4l0 45.4 39.3-22.7L41.8 57.4z" />
-				<path fill="#FF66BB" d="M41.8 57.4L2.6 80.2 2.5 34.8 41.8 57.4z" />
-				<path fill="#94C0E9" d="M41.8 57.4l39.4 22.7 -0.1-45.4L41.8 57.4z" />
-				<path fill="#F29D91" d="M41.8 57.4l-0.2 45.4L2.4 79.9 41.8 57.4z" />
-				<path fill="#43BFAB" d="M41.8 57.4l39.4-22.5L42 12 41.8 57.4z" />
-				<ellipse transform="matrix(-0.866 0.5 -0.5 -0.866 106.8058 87.3056)" fill="#D82113" cx="41.7" cy="58" rx="8.9" ry="8.9" />
+			<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 81.942284 93.999998">
+				<g transform="translate(1 -193.42)">
+					<g transform="matrix(2.25 0 0 2.25 .69932 194.75)">
+						<path fill="#a97ac6" d="m17.454 20.295-17.321 10v-20z" />
+						<path transform="matrix(.26344 .45629 -.45629 .26344 13.599 -11.788)" d="m56.394 24.11-32.874 18.98v-37.96z" fill="#f59c66" />
+						<path transform="matrix(-.26344 .45629 -.45629 -.26344 43.312 .91496)" fill="#68d0bf" d="m56.394 24.11-32.874 18.98v-37.96z" />
+						<path transform="scale(-1)" d="m-17.454-20.295-17.321 10v-20z" fill="#df6eab" />
+						<path transform="matrix(-.26344 -.45629 .45629 -.26344 30.92 -21.788)" fill="#69b3e7" d="m-56.394-62.07-32.874 18.98v-37.96z" />
+						<path transform="matrix(.26344 -.45629 .45629 .26344 60.632 10.915)" d="m-56.394-62.07-32.874 18.98v-37.96z" fill="#f5e64f" />
+						<circle cx="-26.657" transform="rotate(225)" cy="-2.102" r="3.9515" fill="#b72218" />
+					</g>
+				</g>
 			</svg>
 		</div>
 	);
---
+++ hub.1.0.5/src/components/Brands/_backup_BrandLogoOnly.js
@@ -0,0 +1,22 @@
+
+// ----- IMPORTS
+
+import * as React from 'react';
+
+// ----- COMPONENT
+
+const BrandLogoOnly = props =>
+	(
+		<div className="svg-brand--logo-only mos-react-component-root">
+			<svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 118 118" enableBackground="new 0 0 118 118" xmlSpace="preserve">
+				<path fill="#FEBE00" d="M41.8 57.4L2.5 34.7 41.8 12 41.8 57.4z" />
+				<path fill="#AC59FF" d="M41.8 57.4l0 45.4 39.3-22.7L41.8 57.4z" />
+				<path fill="#FF66BB" d="M41.8 57.4L2.6 80.2 2.5 34.8 41.8 57.4z" />
+				<path fill="#94C0E9" d="M41.8 57.4l39.4 22.7 -0.1-45.4L41.8 57.4z" />
+				<path fill="#F29D91" d="M41.8 57.4l-0.2 45.4L2.4 79.9 41.8 57.4z" />
+				<path fill="#43BFAB" d="M41.8 57.4l39.4-22.5L42 12 41.8 57.4z" />
+				<ellipse transform="matrix(-0.866 0.5 -0.5 -0.866 106.8058 87.3056)" fill="#D82113" cx="41.7" cy="58" rx="8.9" ry="8.9" />
+			</svg>
+		</div>
+	);
+export default BrandLogoOnly;
