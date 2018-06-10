6f33047d18a0112a442b463fdcdce3ea36cd85cc
James T Baker <skipbaker@protonmail.com>
2018-06-10 17:10:08 -0400

    Dev phase

--- hub.1.0.5/src/components/HcPushedItems/HcPushedItemsLink.js
+++ hub.1.0.5/src/components/HcPushedItems/HcPushedItemsLink.js
@@ -6,7 +6,7 @@ import * as React from 'react';
 // ----- COMPONENT
 
 const HcPushedDocsLink = props => (
-	<li id={`hc-pushed-items-item_${props.listItemId}`} className="hc-pushed-items-item mos-react-component-root">
+	<li id={`hc-pushed-items-item_${props.listItemId}`} className={`hc-pushed-items-item ${props.listItemContent.type} mos-react-component-root`}>
 		<a href={props.listItemContent.url} className={`hc-pushed-items-item-link ${props.listItemContent.type}`}>{props.listItemContent.anchorText}</a>
 	</li>
 );
