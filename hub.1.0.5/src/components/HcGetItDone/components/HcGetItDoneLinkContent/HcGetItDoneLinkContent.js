b74c440259f17073f9b440eb6a8cff945ce71597
James T Baker <skipbaker@protonmail.com>
2018-06-20 19:00:21 -0400

    Dev phase

--- hub.1.0.5/src/components/HcGetItDone/components/HcGetItDoneLinkContent/HcGetItDoneLinkContent.js
+++ hub.1.0.5/src/components/HcGetItDone/components/HcGetItDoneLinkContent/HcGetItDoneLinkContent.js
@@ -6,9 +6,9 @@ import * as React from 'react';
 // ----- COMPONENT
 
 const HcGetItDoneLinkContent = props => (
-	<div className="hc-get-it-done-item-cotnent mos-react-component-root">
-		<p><a href={props.listItemContent.url} className={`hc-get-it-done-item-link ${props.listItemContent.type}`}>{props.listItemContent.anchorText}</a></p>
-		<p>{props.listItemContent.description}</p>
+	<div className="hc-get-it-done-item-content mos-react-component-root">
+		<p className="hc-get-it-done-item-link-container"><a href={props.listItemContent.url} className={`hc-get-it-done-item-link ${props.listItemContent.type}`}>{props.listItemContent.anchorText}</a></p>
+		<p className="hc-get-it-done-item-description-container">{props.listItemContent.description}</p>
 	</div>
 );
 
