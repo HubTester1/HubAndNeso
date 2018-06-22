0b241a6c6ce8bbe6df3aae855358f7958fc1c22e
James T Baker <skipbaker@protonmail.com>
2018-06-21 23:43:27 -0400

    Dev phase

--- hub.1.0.5/src/components/HcMessages/components/HcMessageFilePreview/HcMessageFilePreview.js
+++ hub.1.0.5/src/components/HcMessages/components/HcMessageFilePreview/HcMessageFilePreview.js
@@ -11,49 +11,11 @@ import MOSUtilities from '../../../../services/MOSUtilities';
 export default class HcMessagesFilePreview extends React.Component {
 	constructor(props) {
 		super(props);
-		// this.ReturnFileSizeToReport = this.ReturnFileSizeToReport.bind(this);
-		/* this.ReturnFileSizeFormattedToOneSignificantDigit = 
-			this.ReturnFileSizeFormattedToOneSignificantDigit.bind(this); */
 	}
-	/* ReturnFileSizeFormattedToOneSignificantDigit(fileSize) {
-		let fileSizeString = fileSize.toFixed(1).toString();
-		const quantityOfDigitsInFileSizeString = fileSizeString.length;
-		const lastDigitInFileSizeString = fileSizeString.charAt(quantityOfDigitsInFileSizeString - 1);
-		if (lastDigitInFileSizeString == '0') {
-			fileSizeString = fileSizeString.substring(0, quantityOfDigitsInFileSizeString - 2);
-		}
-		return fileSizeString;
-	} */
-	/* ReturnFileSizeToReport(rawFileSize) {
-		// perform some file size conversions
-		const fileSizeInKB = rawFileSize / 1000;
-		const fileSizeInMB = fileSizeInKB / 1000;
-		const fileSizeInGB = fileSizeInMB / 1000;
-
-		// set fileSizeToReport to the first file size conversion to be less than 4 digits
-		let fileSizeToReport = '';
-		if (Math.round(fileSizeInKB) > 0 && Math.round(fileSizeInKB) < 1000) {
-			fileSizeToReport = fileSizeInKB;
-			fileSizeToReport = this.ReturnFileSizeFormattedToOneSignificantDigit(fileSizeToReport);
-			fileSizeToReport += ' KB';
-		} else if (Math.round(fileSizeInMB) > 0 && Math.round(fileSizeInMB) < 1000) {
-			fileSizeToReport = fileSizeInMB;
-			fileSizeToReport = this.ReturnFileSizeFormattedToOneSignificantDigit(fileSizeToReport);
-			fileSizeToReport += ' MB';
-		} else if (Math.round(fileSizeInGB) > 0 && Math.round(fileSizeInGB) < 1000) {
-			fileSizeToReport = fileSizeInGB;
-			fileSizeToReport = this.ReturnFileSizeFormattedToOneSignificantDigit(fileSizeToReport);
-			fileSizeToReport += ' GB';
-		} else {
-			fileSizeToReport = `${rawFileSize} bytes`;
-		}
-		return fileSizeToReport;
-	} */
-
 	render() {
 		// console.log(this.props.imageContent);
 		// if this image was uploaded successfully (error is false)
-		if (!this.props.imageContent.error) {
+		if (!this.props.imageContent.error && !this.props.imageContent.migratedFromQuark) {
 			const backgroundImageUrl = MOSUtilities.ReplaceAll(' ', '%20', this.props.imageContent.urlSmall);
 			const imagePreviewStyles = {
 				backgroundImage: `url(${backgroundImageUrl})`,
@@ -92,6 +54,16 @@ export default class HcMessagesFilePreview extends React.Component {
 				</a>
 			);
 		}
+		if (!this.props.imageContent.error && this.props.imageContent.migratedFromQuark) {
+			return (
+				<div id="new-message-image-preview-impossible-message" className="mos-react-form-warning-message banner banner--warning">
+					<p className="banner__text">
+						Files migrated from Quark cannot be edited. However, you can set this message's 
+						expiration to a past date and create a new message.
+					</p>
+				</div>
+			);
+		}	
 		// if this image was NOT uploaded successfully (error is true)
 		return (
 			<div className="hc-messages-images-image image-upload-error mos-react-component-root">
