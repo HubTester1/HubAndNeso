979c92dc43ee594ca676c61f7c593ed8fab9f69e
James Baker <jbaker@FIN-0-OSX-1659.local>
2019-03-22 15:57:42 -0400

    Created custom commmand bars

--- hub.1.0.5/src/components/CommandBar/CommandBar.js
+++ hub.1.0.5/src/components/CommandBar/CommandBar.js
@@ -3,21 +3,48 @@
 
 import * as React from 'react';
 import Collapsible from '../Collapsible/Collapsible';
+import CommandBarItems from '../CommandBar/CommandBarItems';
 
 // ----- COMPONENT
 
 export default (props) => {
-	console.log('CB Props', props);
+	// assume we won't collapse
+	let collapse = false;
+	// if there are 2+ items to render; there's no point in collapsing only one item
+	if (props.items[1]) {
+		// if this screenType is one of those on which to collapse
+		props.collapseOnScreenTypes.forEach((screenTypeToCollapseOnValue) => {
+			if (screenTypeToCollapseOnValue === props.screenType) {
+				// indicate collapse
+				collapse = true;
+			}
+		});
+	}
 	return (
 		<div className="command-bar mos-react-component-root">
-			{/* <Collapsible
-			textCollapsed="About this background image"
-			textExpanded="Hide info about this image"
-			buttonClassName="hc-background-info__button"
-			buttonPosition="afterContent"
-		>
+			{
+				props.items[0] && collapse && 
 
-		</Collapsible> */}
+				<Collapsible
+					textCollapsed={`Reveal ${props.scopeName} commands`}
+					textExpanded={`Hide ${props.scopeName} commands`}
+					iconNameCollapsed="More"
+					iconNameExpanded="More"
+					buttonClassName="command-bar__collapsible-button"
+					buttonPosition="beforeContent"
+				>
+					<CommandBarItems
+						items={props.items}
+					/>
+				</Collapsible>
+			}
+			{
+				props.items[0] && !collapse &&
+
+				<CommandBarItems
+					items={props.items}
+				/>
+			}
 		</div>
 	); 
 };
