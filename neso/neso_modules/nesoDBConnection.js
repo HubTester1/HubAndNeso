caebf0b1ded826f7bf8db8af3e389bc21151d3b7
James Baker <jbaker@FIN-0-OSX-1659.local>
2019-08-12 07:29:11 -0400

    HUB-172

--- neso/neso_modules/nesoDBConnection.js
+++ neso/neso_modules/nesoDBConnection.js
@@ -3,7 +3,10 @@
 
 const monk = require('monk');
 
+const connectTimeoutMS = 30000;
+const socketTimeoutMS = 30000;
+
 
 // ----- EXPORT DB CONNECTION
 
-module.exports = monk(`${process.env.dbUser}:${process.env.dbPass}@${process.env.dbHost}:${process.env.dbPort}/${process.env.dbName}`);
+module.exports = monk(`${process.env.dbUser}:${process.env.dbPass}@${process.env.dbHost}:${process.env.dbPort}/${process.env.dbName}?connectTimeoutMS=${connectTimeoutMS}&socketTimeoutMS=${socketTimeoutMS}`);
