37d89c3ce921eed5df5d9f972fc7a031e29b19fd
jamesTbaker <jamesTbaker@outlook.com>
2019-12-13 14:01:40 -0500

    Backed out screen selection logic

--- hub.2.1/components/Screens/Home.js
+++ hub.2.1/components/Screens/Home.js
@@ -3,15 +3,11 @@ import { connect } from 'react-redux';
 import Utilities from '../../services/Utilities';
 import * as PartialScreenRegistry from '../PartialScreens/Home/HomeRegistry';
 
-const screenKey = 'Home';
-
 const PartialScreen = ({ screenType, sData }) => {
 	// all partial screens are imported as PartialScreenRegistry, above; create component equal
 	// 		to the partial screen component specified in sData; render said component below
-	const selctedPartialScreenName = sData.p ?
-		Utilities.ReturnStringWithInitialCapital(sData.p) :
-		sData.screens[screenKey].defaultPartial;
-	const SelectedPartialScreen = PartialScreenRegistry[selctedPartialScreenName];
+	const SelectedPartialScreen = 
+		PartialScreenRegistry[Utilities.ReturnStringWithInitialCapital(sData.p)];
 	return (<SelectedPartialScreen screenType />);
 };
 
--- hub.2.1/services/Screen.js
+++ hub.2.1/services/Screen.js
@@ -5,36 +5,51 @@ module.exports = {
 	// ReturnMediumMax: () => 1024,
 	ReturnMediumMax: () => 1120,
 	ReturnSmallMax: () => 767,
-	ReturnSData: (s = 'home', p) => (
-		{
-			s,
-			p,
-			screens: {
-				home: {
-					title: 'Home',
-					defaultPartial: 'todayAndRecent',
-					partials: {
-						todayAndRecent: {
-							title: 'Today & Recent',
-						},
-						pinned: {
-							title: 'Pinned',
-						},
+	ReturnSData: (s, p) => {
+		const screens = {
+			home: {
+				title: 'Home',
+				defaultPartial: 'todayAndRecent',
+				partials: {
+					todayAndRecent: {
+						title: 'Today & Recent',
 					},
-				},
-				messages: {
-					title: 'Messages',
-					defaultPartial: 'announcements',
-					partials: {
-						announcements: {
-							title: 'Announcements',
-						},
-						classifieds: {
-							title: 'Classifieds',
-						},
+					pinned: {
+						title: 'Pinned',
 					},
 				},
 			},
-		}
-	),
+			messages: {
+				title: 'Messages',
+				defaultPartial: 'announcements',
+				partials: {
+					announcements: {
+						title: 'Announcements',
+					},
+					classifieds: {
+						title: 'Classifieds',
+					},
+				},
+			},
+		};
+		// select home screen if one isn't specified; do so here
+		// 		to keep logic near selection of partial screen
+		// eslint-disable-next-line no-unneeded-ternary
+		const selectedScreen = s ? s : 'home';
+		// if partial isn't specified or if partial isn't actually 
+		// 		possible for (a child of) selected screen, then use
+		// 		screen's default partial
+		const selectedPartialScreen = 
+			(!p || !screens[selectedScreen].partials[p]) ? 
+				screens[selectedScreen].defaultPartial :
+				p;
+		
+		return (
+			{
+				s: selectedScreen,
+				p: selectedPartialScreen,
+				screens,
+			}
+		);
+	},
 };
