140b4243a50489573ee7ab98956ca3f988ecc039
jamesTbaker <jamesTbaker@outlook.com>
2020-02-21 13:42:27 -0500

    Marker update

--- hub.1.0.5/src/components/HcContainer/HcContainer.js
+++ hub.1.0.5/src/components/HcContainer/HcContainer.js
@@ -190,7 +190,7 @@ class HcContainer extends React.Component {
 }
 
 // eslint-disable-next-line no-console
-console.log('hc m4');
+console.log('hc m1');
 const screenShower = setInterval(ShowScreen, 500);
 if (EnvironmentDetector.ReturnIsHCScreen()) {
 	document.querySelector('head title').textContent = 'The Hub - Home';
